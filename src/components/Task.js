e1c94cfd27e41c7e1197b685e07f646e3e67d974
Mgridgelabs <mainaianndungu@gmail.com>
2024-08-04 23:00:28 +0300

    completes hooks state events mini projects

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function Task() {
+function Task({ task, text, category, onDeleteTask }) {
+  const taskText = task ? task.text : text;
+  const taskCategory = task ? task.category : category;
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+       {taskCategory && <div>{taskCategory}</div>}
+       <div>{taskText}</div>
+      <button className="delete" onClick={() => onDeleteTask(task.text)}>X</button>
     </div>
   );
 }
